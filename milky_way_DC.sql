1031ad97a9f19b603f87ebd82ecaf7485ed32f7f
Paulina Leal <lealherrerapaulina@gmail.com>
2025-08-08 17:57:41 -0600

    Data Cleaning Begins!

--- milky_way_DC.sql
+++ milky_way_DC.sql
@@ -1,1 +1,39 @@
 -- MILKY WAY: Data Cleaning
+
+SELECT *
+FROM celestial_body;
+
+#Creating a copy of celestial_body
+CREATE TABLE cb
+LIKE celestial_body;
+
+SELECT * 
+FROM cb;  
+
+INSERT cb
+SELECT *
+FROM celestial_body;
+
+SELECT *
+FROM cb; 
+
+#Checking of duplicates
+SELECT *, 
+	ROW_NUMBER() OVER(PARTITION BY name) AS row_num
+FROM cb; 
+
+#Removing duplicates
+WITH duplicate_cte AS(
+	SELECT *,
+		ROW_NUMBER() OVER(PARTITION BY name) AS row_num
+    FROM cb
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+
+
+
+
+
